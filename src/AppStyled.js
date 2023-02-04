bcd1a347fb0774c7df980b3e6280c8c795000049
Cristiano Ap Lázaro <cristianoalazaro@hotmail.com>
2023-02-04 12:24:43 -0300

    Initial

---
+++ src/AppStyled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+height: 100vh;
+display: flex;
+background-color: #FF0000;
+`
+
+export const Menu = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+background-color: #136713;
+width: 80px;
+`
+
+export const PageBody = styled.div`
+display: flex;
+background-color: #0000FF;
+flex: 1;
+`
